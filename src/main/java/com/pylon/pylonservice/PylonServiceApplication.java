d24c7e24b8f022fb1974cbd40862523cd19fd447
Jason <leetprojects@gmail.com>
2020-05-28 17:52:25 -0700

    Make PylonServiceApplication extend SpringBootServletInitializer

--- src/main/java/com/pylon/pylonservice/PylonServiceApplication.java
+++ src/main/java/com/pylon/pylonservice/PylonServiceApplication.java
@@ -2,13 +2,14 @@ package com.pylon.pylonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class PylonServiceApplication {
+public class PylonServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PylonServiceApplication.class, args);
