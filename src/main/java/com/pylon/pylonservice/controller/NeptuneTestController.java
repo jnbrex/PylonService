3faa096852def4284013d78e3b4083652e6ea3f7
Jason <leetprojects@gmail.com>
2020-07-10 00:01:26 -0700

    Remove unneeded request body for /neptuneTest/vertexCount

--- src/main/java/com/pylon/pylonservice/controller/NeptuneTestController.java
+++ src/main/java/com/pylon/pylonservice/controller/NeptuneTestController.java
@@ -1,13 +1,11 @@
 package com.pylon.pylonservice.controller;
 
-import com.pylon.pylonservice.model.requests.RegisterRequest;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +15,7 @@ public class NeptuneTestController {
     private GraphTraversalSource rG;
 
     @GetMapping(value = "/neptuneTest/vertexCount")
-    public ResponseEntity<?> vertexCount(@RequestBody final RegisterRequest registerRequest) {
+    public ResponseEntity<?> vertexCount() {
         long count = rG.V().count().next();
 
         return new ResponseEntity<>(
