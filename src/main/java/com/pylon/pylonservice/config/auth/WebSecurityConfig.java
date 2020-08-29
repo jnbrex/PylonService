a032ccee7441bc857400527e09055a2b33dd265d
Jason <leetprojects@gmail.com>
2020-08-28 22:04:59 -0700

    Fix disallowing x-requested-with header for CORS

--- src/main/java/com/pylon/pylonservice/config/auth/WebSecurityConfig.java
+++ src/main/java/com/pylon/pylonservice/config/auth/WebSecurityConfig.java
@@ -67,7 +67,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList(
+            "Authorization",
+            "Cache-Control",
+            "Content-Type",
+            "X-Requested-With"
+        ));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
